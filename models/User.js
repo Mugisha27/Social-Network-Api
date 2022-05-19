d0b5e102368d99c9b367b69ff1fbceba3b2d691a
chris Mugisha <chrismugisha@chriss-MacBook-Air.local>
2022-05-19 13:28:16 -0700

    more changes

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const profileSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            // Email Match Regex
+          
             match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
         },
         thoughts: [
